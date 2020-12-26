e7e32afa9a92c3de05f01d24ba89389499ff8d5a
Guy Tepper <guytepper@gmail.com>
2020-12-26 17:54:36 +0200

    New event form

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,3 +1,4 @@
 export { default as Login } from './Login';
 export { default as Home } from './Home';
 export { default as CreatePost } from './Feed/CreatePost';
+export { default as CreateEvent } from './Events/CreateEvent';
