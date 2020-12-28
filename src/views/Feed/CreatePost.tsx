42cb3abbf035c971147a20dce0f917fd54a71c43
Guy Tepper <guytepper@gmail.com>
2020-12-28 03:40:26 +0200

    Added slate text editor

--- src/views/Feed/CreatePost.tsx
+++ src/views/Feed/CreatePost.tsx
@@ -1,9 +1,22 @@
+// @ts-nocheck
+
+import { useState, useMemo } from 'react';
 import { Form, Input, Select, Button } from 'antd';
 import { useFirestore, useFirestoreCollectionData } from 'reactfire';
+import { firestore } from '../../firebase';
 import { Organization } from '../../types/firestore';
+
+import escapeHtml from 'escape-html';
+import { createEditor, Node, Text } from 'slate';
+import { Slate, Editable, withReact } from 'slate-react';
 
 const { Option } = Select;
 const { TextArea } = Input;
+
+type PostDocument = {
+  organizationId: string;
+  content: string;
+};
 
 const layout = {
   labelCol: { span: 8 },
@@ -13,8 +26,53 @@ const tailLayout = {
   wrapperCol: { offset: 12 },
 };
 
+const serialize = (node: Node): any => {
+  if (Text.isText(node)) {
+    return escapeHtml(node.text);
+  }
+
+  const children = node.children.map((n) => serialize(n)).join('');
+
+  switch (node.type) {
+    case 'quote':
+      return `<blockquote><p>${children}</p></blockquote>`;
+    case 'paragraph':
+      return `<p>${children}</p>`;
+    case 'link':
+      return `<a href="${escapeHtml(node.url)}">${children}</a>`;
+    default:
+      return children;
+  }
+};
+
 function CreatePost() {
   const { data: organizations }: { data: Organization[] } = useFirestoreCollectionData(useFirestore().collection('organizers'));
+  const [value, setValue] = useState<Node[]>([
+    {
+      type: 'paragraph',
+      children: [{ text: '' }],
+    },
+  ]);
+  const editor = useMemo(() => withReact(createEditor()), []);
+
+  const onFinish = async (values: PostDocument) => {
+    try {
+      const content = serialize(editor);
+
+      firestore
+        .collection('posts')
+
+        .add({
+          ...values,
+          content,
+        });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
 
   if (!organizations || organizations.length === 0) {
     return <p>טוענת..</p>;
@@ -22,8 +80,8 @@ function CreatePost() {
 
   return (
     <div className="form-wrapper">
-      <Form {...layout} name="feed-post" initialValues={{ remember: true }}>
-        <Form.Item label="כותב הפוסט" name="author">
+      <Form {...layout} name="feed-post" onFinish={onFinish} onFinishFailed={onFinishFailed} initialValues={{ remember: true }}>
+        <Form.Item label="כותב הפוסט" name="organizationId">
           <Select>
             {organizations.map((org) => (
               <Option value={org.NO_ID_FIELD} key={org.NO_ID_FIELD}>
@@ -33,9 +91,13 @@ function CreatePost() {
             ))}
           </Select>
         </Form.Item>
-        <Form.Item label="תוכן" name="content">
-          <TextArea autoSize={{ minRows: 3, maxRows: 5 }} placeholder="תוכן הפוסט" />
-        </Form.Item>
+        {/* <Form.Item label="תוכן" name="content" noStyle> */}
+        <label>תוכן:</label>
+        <Slate editor={editor} value={value} onChange={(newValue) => setValue(newValue)}>
+          <Editable style={{ width: 200, padding: 10, border: '1px solid #d9d9d9' }} />
+        </Slate>
+        {/* <TextArea autoSize={{ minRows: 3, maxRows: 5 }} placeholder="תוכן הפוסט" /> */}
+        {/* </Form.Item> */}
         <Form.Item {...tailLayout}>
           <Button type="primary" htmlType="submit">
             יצירת פוסט
